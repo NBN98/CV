d4bc2c3f4b9c004a75bb1448dde468fb52ea6b76
Florian Butsch <butsch@itq.de>
2020-06-24 23:51:02 +0200

    added postprocessing to changed parameters

--- TestDetect.m
+++ TestDetect.m
@@ -3,10 +3,11 @@
 videoSource = VideoReader('TestVideo.avi');
 
 detector = vision.ForegroundDetector(...
-       'NumTrainingFrames', 5, ...
-       'InitialVariance', 30*30);
+       'NumTrainingFrames', 500, ...
+       'InitialVariance', 50*50, 'MinimumBackgroundRatio', 0.7);
 
-%to try later, not used here
+%to try later
+
 area = vision.BlobAnalysis(...
    'CentroidOutputPort', false, 'AreaOutputPort', true, ...
    'BoundingBoxOutputPort', false, ...
@@ -18,14 +19,54 @@ videoPlayer = vision.VideoPlayer();
 while hasFrame(videoSource)
      frame  = readFrame(videoSource);
      %to try later, not used here
-     fgMask = detector(frame);
+     %fgMask = detector(frame);
      %area = blob(fgMask);
+     
+     
+     %Preprocessing before 
+     frame_hsv = rgb2hsv(frame);
+    % "30% more" saturation:
 
+    frame_hsv(:, :, 2) = frame_hsv(:, :, 2) * 1.3;
+
+    frame_hsv(frame_hsv > 1) = 1;  % Limit values
+
+    frame_saturated = hsv2rgb(frame_hsv);
+     
      
     
+    
+    %actual detector
      foreground = step(detector, frame);
      
-     videoPlayer(foreground);
-     pause(0.1);
+     
+     
+     %to postprocess
+     FilteredImg = medfilt2(foreground, [5 5]);
+    [L num] = bwlabel(FilteredImg);
+    %get the regions properties this can be used to get rid of the door
+    %frame section on the right
+    STATS = regionprops(L, "all");
+    cc = [];
+    removed = 0;
+    for i=1:num
+       dd= STATS(i).Area;
+       if (dd < 1000)
+           L(L==i)=0;
+           removed=removed+1;
+           num=num-1;
+           
+       end
+    end
+    [L2 num2] = bwlabel(L);
+     foreground_postprocess = L2;
+     
+     frame_overlap(:,:,1) = uint8(foreground_postprocess) .* frame(:,:,1);
+     frame_overlap(:,:,2) = uint8(foreground_postprocess) .* frame(:,:,2);
+     frame_overlap(:,:,3) = uint8(foreground_postprocess) .* frame(:,:,3);
+     
+     videoPlayer(frame_overlap);
+     %videoPlayer(foreground_postprocess);
+     pause(0.01);
 end
 release(videoPlayer);
