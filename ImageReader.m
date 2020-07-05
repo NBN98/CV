68145688b615218901886e2e89734f0334a386de
fsmajli <43210697+fsmajli@users.noreply.github.com>
2020-07-05 20:49:13 +0200

    Add files via upload

--- ImageReader.m
+++ ImageReader.m
@@ -1,19 +1,27 @@
-classdef ImageReader
+classdef ImageReader < handle
   % Add class description here
   %
   %
+ 
   properties
       src='';
       L=0;
       R=0;
       start=0;
       N=0;
-  end
+      joint_path_L='';
+      joint_path_R='';
+      loop=0;
+      counter =0;
       
+      
+      
+  end
+  
   methods
       % constructor with input parser
       function obj = ImageReader(src, L, R, varargin)
-          
+         
           p = inputParser;
           defaultstart = 0;
           defaultN = 1;
@@ -41,43 +49,236 @@ classdef ImageReader
           obj.start=p.Results.start;
           obj.N=p.Results.N;
           
+          
+          
           if obj.L == 1
               % sub Directory 1 (search whether a folder has 'C1' in
               % name)
               path_L = dir(strcat(obj.src, '\*C1*')); %returns struct array
-              joint_path_L=strcat(path_L.folder,'\', path_L.name);
-              disp(joint_path_L)
-
+              obj.joint_path_L=strcat(path_L.folder,'\', path_L.name);
+              
               
           else
               % sub directory 2
               path_L = dir(strcat(obj.src, '\*C2*')); %returns struct array
-              joint_path_L=strcat(path_L.folder,'\', path_L.name);
-              disp(joint_path_L)
+              obj.joint_path_L=strcat(path_L.folder,'\', path_L.name);
+              
               
           end
           
           if R ==2
               %sub directory 2
               path_R = dir(strcat(obj.src, '\*C2*')); 
-              joint_path_R=strcat(path_R.folder,'\', path_R.name);
-              disp(joint_path_R)
+              obj.joint_path_R=strcat(path_R.folder,'\', path_R.name);
+              
           else
              % sub directory 3
              path_R = dir(strcat(obj.src, '\*C3*')); 
-             joint_path_R=strcat(path_R.folder,'\', path_R.name);
-             disp(joint_path_R)
+             obj.joint_path_R=strcat(path_R.folder,'\', path_R.name);
+             
           
           end
-      
-          function obj = next()
-              % load images at start variable and increment by N
-              %path = dir(strcat(obj.src, '\*.png'))
-              %for
           
-          end
-      
       end
+      
 
+      
+           
+          function [left, right, l]=next(obj)
+              joint_path_L = obj.joint_path_L;
+             
+              %loads the images into a struct array
+              images_L = dir(strcat(obj.joint_path_L, '\*.jpg'));
+              images_R = dir(strcat(obj.joint_path_R, '\*.jpg'));
+              
+             
+              %saves the image properties to workspace
+              %assignin('base','image_L',images_L);
+              %assignin('base','image_R', images_R);
+
+            
+              % for every call, we increase the the class property counter
+              obj.counter = obj.counter+1;
+              ImageArray_L=cell(1, 2292);
+              ImageArray_R=cell(1, 2292);
+              index=1;
+%               for i=50:70
+%                   
+%                   ImageArray{tt}=imread(strcat(obj.joint_path_L, '\', images_L(i).name));
+%                   tt=tt+1;
+%               end
+              
+           
+              %whos ImageArray
+              
+%               for i=2:length(ImageArray)
+%                  if ~isempty(ImageArray{1,i})
+%                    Image0 =  ImageArray{1,1};
+%                    tmp = ImageArray{1,i};
+%                    left=cat(3, Image0, tmp);
+%                    assignin('base','left', left);
+%                  else
+%                      break
+%                  end
+%               end
+               
+          try    
+              if obj.counter == 1
+                  %if loop is not 1, then start at the
+                  %start value
+                  if obj.loop ~= 1
+                      if obj.start==0
+                          % added +1 to obj.start, becauce if start=0 -->
+                          % array ind ex starts at 1
+                          
+                          for i=obj.start+1:obj.start+1+obj.N
+                              %disp(images_L(i).name)
+                              % ImageArray{i}=imread(strcat(obj.joint_path_L, '\', images_L(i).name));
+                              ImageArray_L{index}=imread(strcat(obj.joint_path_L, '\', images_L(i).name));
+                              ImageArray_R{index}=imread(strcat(obj.joint_path_R, '\', images_L(i).name));
+                              index=index+1;
+                              %obj.counter = i;
+                              %disp(strcat(obj.joint_path_R, '\', images_L(i).name))
+                              
+                          end
+                          l=obj.loop;
+                          %assignin('base','ImageArray_L', ImageArray_L);
+                          %assignin('base','ImageArray_R', ImageArray_R);
+                          left=cat(3, ImageArray_L{:}); %To show the image use figure, then montage(left)
+                          right=cat(3, ImageArray_R{:});
+                          %assignin('base','left', left);
+                          %assignin('base','right', right);
+                          
+                      % if start is not zero    
+                      else
+                          for i=obj.start+1:obj.start+obj.N+1
+                              %disp('K');
+                              %disp(images_L(i).name)
+                              % ImageArray{i}=imread(strcat(obj.joint_path_L, '\', images_L(i).name));
+                              ImageArray_L{index}=imread(strcat(obj.joint_path_L, '\', images_L(i).name));
+                              ImageArray_R{index}=imread(strcat(obj.joint_path_R, '\', images_L(i).name));
+                              index=index+1;
+                              %disp(strcat(obj.joint_path_R, '\', images_L(i).name))
+                              obj.counter = i-obj.N;
+                              %disp(images_L(i).name);
+                              if images_L(i).name == images_L(end).name
+                                  obj.counter = 0;
+                                  i=obj.counter;
+                                  obj.loop=1;
+                                  %l = obj.loop;
+                                  %disp(obj.loop);
+                                  
+                                  
+                                   %assignin('base','ImageArray_L', ImageArray_L);
+                                   %assignin('base','ImageArray_R', ImageArray_R);
+%                                   left=cat(3, ImageArray_L{:}); %To show the image use figure, then montage(left)
+%                                   right=cat(3, ImageArray_R{:});
+%                                   assignin('base','left', left);
+%                                   assignin('base','right', right);
+                                  break;
+                                  
+                                  
+                              else
+                                  continue;
+                                  
+                              end
+                              
+                          end
+                            l=obj.loop;
+                            %assignin('base','ImageArray_L', ImageArray_L);
+                            %assignin('base','ImageArray_R', ImageArray_R);
+                            left=cat(3, ImageArray_L{:}); %To show the image use figure, then montage(left)
+                            right=cat(3, ImageArray_R{:});
+                            %assignin('base','left', left);
+                            %assignin('base','right', right);
+                      end
+                      
+                      % if loop is equal to 1, start at the beginning even
+                      % though the initial start variable has a different
+                      % start
+                  else
+                      for i=obj.counter:obj.counter+obj.N
+                          %disp(images_L(i).name)
+                          ImageArray_L{index}=imread(strcat(obj.joint_path_L, '\', images_L(i).name));
+                          ImageArray_R{index}=imread(strcat(obj.joint_path_R, '\', images_L(i).name));
+                          index=index+1;
+                          %obj.counter = i;
+                          %disp(strcat(obj.joint_path_R, '\', images_L(i).name));
+                          %disp(obj.loop);
+                          
+                          obj.loop = 0;     %reset the loop value
+                          %l = obj.loop;
+                          %disp(obj.loop)
+                      end
+                      l=obj.loop;
+                      %assignin('base','ImageArray_L', ImageArray_L);
+                      %assignin('base','ImageArray_R', ImageArray_R);
+                      left=cat(3, ImageArray_L{:}); %To show the image use figure, then montage(left)
+                      right=cat(3, ImageArray_R{:});
+                      %assignin('base','left', left);
+                      %assignin('base','right', right);
+                  end
+                  
+                  
+              else %if the obj.counter is not 1
+                  
+                  
+                  for j=obj.counter:obj.counter+obj.N
+                      %disp(images_L(j).name)
+                      ImageArray_L{index}=imread(strcat(obj.joint_path_L, '\', images_L(j).name));
+                      ImageArray_R{index}=imread(strcat(obj.joint_path_R, '\', images_L(j).name));
+                      index=index+1;
+                      %obj.counter = j;
+                      %disp(strcat(obj.joint_path_R, '\', images_L(j).name))
+                      % if the loaded image is the last one, we should
+                      % start at the beginning, since obj.loop is only
+                      % defined here equal to 1
+                      if images_L(j).name == images_L(end).name
+                          obj.counter = 0;
+                          j=obj.counter;
+                          obj.loop=1;
+                          
+                          %disp(images_L(j).name);
+%                           assignin('base','ImageArray_L', ImageArray_L);
+%                           assignin('base','ImageArray_R', ImageArray_R);
+%                           left=cat(3, ImageArray_L{:}); %To show the image use figure, then montage(left)
+%                           right=cat(3, ImageArray_R{:});
+%                           assignin('base','left', left);
+%                           assignin('base','right', right);
+                          break;
+                          
+                          
+                      else
+                          continue;
+                          
+                      end
+                      
+                  end
+                  l=obj.loop;
+                  %assignin('base','ImageArray_L', ImageArray_L);
+                  %assignin('base','ImageArray_R', ImageArray_R);
+                  left=cat(3, ImageArray_L{:}); %To show the image use figure, then montage(left)
+                  right=cat(3, ImageArray_R{:});
+                  %assignin('base','left', left);
+                  %assignin('base','right', right);
+                      
+              end
+              
+          catch
+              
+              if obj.start > length(images_L)
+                  error('Start value exceeds the number of images. Value should be within 0 and %d.', length(images_L));
+                  
+                  
+              else
+                  error('Errors occur. Check value N or syntax');
+                  
+              end
+          end
+          
+          
+          end
+          
+      
   end
 end
--- render.m
+++ render.m
@@ -1,22 +1,28 @@
-function [result, result2, mask] = render(frame,mask,bg,mode)
+function [result, result2, mask,v2] = render(frame,mask,bg,mode,v)
   % Add function description here
   %
   %
   input = true;
+  %is this while necessary??
+  
+  
   while input
       
       if mode == 'foreground'
           maskedRgbImage = bsxfun(@times, frame, cast(mask, 'like', frame));
+          result=maskedRgbImage;
+          result2=frame;
           input = false;
       elseif mode == 'background'
           maskedRgbImage = bsxfun(@times, frame, cast(~mask, 'like', frame));
+          result=maskedRgbImage;
+          result2=frame;
           input = false;
       elseif mode == 'overlay'
           
           input = false;
           
       elseif mode == 'substitute'
-          disp('gg');
           maskedRgbImage = bsxfun(@times, frame, cast(mask, 'like', frame));
           rgbImage=frame; %imread('C:\Users\noahb\Documents\object_tracking\img\00004384.jpg');
           maskedImage=maskedRgbImage;
@@ -37,6 +43,26 @@ function [result, result2, mask] = render(frame,mask,bg,mode)
           input = false;
       elseif mode == 'bonus'
           
+             videoBack=VideoReader(bg);
+            
+             backframe=read(videoBack,v); %taking single frame of video
+
+             %resizing background into dimensions of input image
+             backframe=imresize(backframe,[size(frame,1) size(frame,2)]);
+             v2=v+1
+             % reseting video when over
+             if v==videoBack.NumberOfFrames+1;
+                 v=1;
+             end
+
+             inv_mask=ones(size(mask))-mask;
+
+             result=frame.*uint8(mask)+backframe.*uint8(inv_mask);% adding for-and background together
+             result2=frame;
+             mask=mask;
+            % videoPlayer(result);
+             %pause(0.01);
+            
           input = false;
       else
           disp('Wrongt input')
