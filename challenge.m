c1a6b1e7ebe0df2472d9b0739fd4090e99e1bdb8
fsmajli <43210697+fsmajli@users.noreply.github.com>
2020-07-06 03:38:00 +0200

    Add files via upload

--- challenge.m
+++ challenge.m
@@ -4,20 +4,110 @@
 tic;
 
 %% Generate Movie
+config
+loop=0;
+v=1;
+ir=ImageReader(src, L, R, start, N);
+%sumImage=calcBack(src, L);
 
-while loop ~= 1
-  % Get next image tensors
 
-  % Generate binary mask
-
-  % Render new frame
+%pre allocate variable
+movie_frames=cell(1, 3000);
+if mode == 'substitute'
+    bg=imread(bg);
+elseif mode =='bonus'
+    bg=VideoReader(bg);
 end
+i=1;
+warning('off','last')
+while loop ~= 1
+    
+    % Set up figure properties:
+    % Enlarge figure to full screen.
+    set(gcf, 'Units', 'Normalized', 'OuterPosition', [0 0 1 1]);
+    % Get rid of tool bar and pulldown menus that are along top of figure.
+    set(gcf, 'Toolbar', 'none', 'Menu', 'none');
+    % Give a name to the title bar.
+    set(gcf, 'Name', 'G21 Computer Vision Challenge', 'NumberTitle', 'Off')
+    
+  % Get next image tensors
+  
+  [left, right]=ir.next();%0.75s
+  
+    loop=0;
+    
+  % Generate binary mask
+  
+  [mask, background]=segmentation(left,right); %sumImage);%1.5s
+  
+  frame=left(:,:,(end-2:end));
+  % Render new frame
+
+
+    
+  [result, result2,mask,v] = render(frame,mask,bg,mode,v);%0.01s up to 0.1 for bonus
+  
+    %v=v2;
+  subplot(3, 3, 1);
+  imshow(result2);
+  drawnow;
+  subplot(3, 3, 2);
+  str= sprintf('Selected Mode: %s', mode);
+  
+  imshow(result);
+  title(str, 'FontSize', fontSize, 'Interpreter', 'None');
+  
+  subplot(3, 3, 3);
+  imshow(mask);
+  
+  subplot(3, 3, 4);
+  grayImage = rgb2gray(result2);
+  diffImage = abs(double(grayImage) - double(background));
+  imshow(diffImage)
+  
+  %Saves the result into the movie_frames cell array
+  movie_frames{i}=result;
+  i=i+1;
+  
+  
+  
+end
+% subplot(3, 3, 1);
+% imshow(result2);
+% 
+% subplot(3, 3, 2);
+% imshow(result);
+% 
+% subplot(3, 3, 3);
+% imshow(mask);
+% 
+% subplot(3, 3, 4);
+% grayImage = rgb2gray(result2); 
+% diffImage = abs(double(grayImage) - double(background));
+% imshow(diffImage)
+
 
 %% Stop timer here
 elapsed_time = toc;
 
 
 %% Write Movie to Disk
+
+
+
+%store=true;
 if store
-  %v = VideoWriter(dst,'Motion JPEG AVI');
+    
+    v = VideoWriter(dst, 'Motion JPEG AVI');
+    %v.Quality=85;
+    for K = 1 : length(movie_frames)
+        if ~isempty(movie_frames{K})
+            movie=movie_frames{K};
+            open(v)
+            writeVideo(v, movie);
+        else
+            break;
+        end
+    end
+    close(v);
 end
--- config.m
+++ config.m
@@ -1,42 +1,49 @@
 %% Computer Vision Challenge 2020 config.m
-
+fontSize = 20;
 %% Generall Settings
-% Group number:
-% group_number = 0;
+% Group number: 21
+ group_number = 21;
 
 % Group members:
-% members = {'Max Mustermann', 'Johannes Daten'};
+ members = {'Noah Binh Nguyen', 'Fatbardh Smajli', 'Roberto Ruano Martinez', 'Florian Butsch', 'Marta Caneda Portela'};
 
 % Email-Address (from Moodle!):
-% mail = {'ga99abc@tum.de', 'daten.hannes@tum.de'};
+ mail = {'noahbinh.nguyen@tum.de', 'fatbardh.sm@tum.de', 'Roberto.ruano@tum.de', 'florian.butsch@tum.de', 'marta.caneda@tum.de'};
 
 
 %% Setup Image Reader
 % Specify Scene Folder
-src = "Path/to/my/ChokePoint/P1E_S1";
+src = "C:\Users\fatis\Desktop\Semester 1 Master\Computer Vision\Bilder\P1E_S1";
 
 % Select Cameras
-% L =
-% R =
+ L =2;
+ R =3;
 
 % Choose a start point
-% start = randi(1000)
+start = 400;
 
 % Choose the number of succseeding frames
-% N =
+%(think 5 works better)
+N = 10;
 
-ir = ImageReader(src, L, R, start, N);
 
 
 %% Output Settings
 % Output Path
 dst = "output.avi";
 
-% Load Virual Background
-% bg = imread("Path\to\my\virtual\background")
+% Select rendering mode (Choose between foreground, background, overlay,
+% substitute, bonus
+mode = "overlay"; 
 
-% Select rendering mode
-mode = "substitute";
+% Load Virual Background
+if mode=="substitute";
+    bg = "C:\Users\fatis\Desktop\Semester 1 Master\Computer Vision\bg.jpg";
+elseif mode=="bonus";
+    bg = "C:\Users\fatis\Desktop\Semester 1 Master\Computer Vision\wavesloop.mp4";
+end
+
+
 
 % Create a movie array
 % movie =
